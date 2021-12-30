b7c6bcfeb5dededa0a8d495061a48ba6b35f7481
ghaidahgg <ghaida_09@outlook.com>
2021-12-30 08:44:15 +0300

    Initial commit

---
+++ demo/src/test/java/com/saib/spring/demo/SpringAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saib.spring.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
